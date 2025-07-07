ea88d3be3c1034948649eff455e6464c064658ee
Harshita <harshita0601.be23@chitkara.edu.in>
2025-07-07 10:10:08 +0530

    done

---
+++ SPRING BOOT/Class 2/account.java
@@ -0,0 +1,19 @@
+// package SPRING BOOT.Class 2;
+
+// public package Datahidding;
+
+// public class Account {
+ 
+	
+// 	private double balance = 5000;
+
+// 	public double getBalance() {
+// 		return balance;
+// 	}
+
+// 	public void setBalance(double balance) {
+// 		this.balance = balance;
+// 	}
+// } {
+    
+// }
---
+++ SPRING BOOT/class 5/collection.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class collection{
+    public static void main(String[] args) {
+        
+        Collection<String> collection1 = new ArrayList<>(); 
+        Collection<String> collection2 = new ArrayList<>(); 
+
+
+        //  1. add()
+        collection1.add("Apple");
+        collection1.add("Banana");
+        collection1.add("Cherry");
+        collection2.add("Banana");
+        collection2.add("Dates");
+
+
+        System.out.println("After add(): " + collection1);
+        collection1.add(null);
+        collection1.add(null);
+        System.out.println("After adding null: " + collection1); // we can add null values multiple times
+
+        collection1.remove("Cherry");
+        System.out.println("After remove(): " + collection1); // removing an element that is cherry
+
+        collection1.remove(null); // removing null
+        collection1.remove(null); // removing null
+
+        System.out.println("After removing null: " + collection1); // removing null
+
+
+        collection1.addAll(collection2); // adding collection2 to collection1
+        System.out.println("After addAll(): " + collection1); // collection2 is empty
+
+
+        collection1.removeAll(collection2); // removing all elements of collection2 from collection1
+        System.out.println("After removeAll(): " + collection1); // collection2 is empty
+
+    }
+}
