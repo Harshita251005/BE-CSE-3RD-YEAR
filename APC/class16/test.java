695c92e246598324a66d68a63a3e4ecc9efdb6a8
Harshita <harshita0601.be23@chitkara.edu.in>
2025-07-21 11:52:58 +0530

    done

--- APC/class16/test.java
+++ APC/class16/test.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -14,5 +15,23 @@ public class test {
         fw.write("This is a test file.\n"); // Write another line
 
         fw.flush();
+
+        FileReader fr = new FileReader(file); // Open the file for reading
+        // System.out.println((char)fr.read()); // Read and print the contents of the file
+        System.out.println(fr.read()); 
+        int i;
+        while ((i = fr.read()) != -1) { // Read until the end of the file
+            System.out.print((char) i); // Print each character
+        }
+        // fr.close(); // Close the FileReader
+        // fw.close(); // Close the FileWriter
+        // file.delete(); // Delete the file after use
+        // f.delete(); // Delete the directory after use
+        // System.out.println("\nFile operations completed successfully.");
+        // Note: Ensure that the file and directory are not in use before deleting them.
+        // This code will create a directory, create a file in it, write to the file
+        // read from the file, and then delete both the file and the directory.
+        // Make sure to handle exceptions properly in production code.
+        // This is a simple demonstration of file operations in Java.
     }
 }
