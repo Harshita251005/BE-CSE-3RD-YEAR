49139f07b851c2aa24691b6944d7d993045159c7
Harshita <harshita0601.be23@chitkara.edu.in>
2025-07-21 12:35:08 +0530

    done

--- APC/class16/test2.java
+++ APC/class16/test2.java
@@ -4,18 +4,18 @@ class test2 {
     public static void main(String[] args) throws IOException {
         // Create directory
         File dir = new File("printwriter");
-        dir.mkdir(); // Create the directory if it does not exist
+        dir.mkdir(); 
 
         // Create file inside the directory
         File file = new File(dir, "file1.txt");
-        file.createNewFile(); // Create the file if it does not exist
+        file.createNewFile();
 
         // Write to file using PrintWriter
         PrintWriter pw = new PrintWriter(file); // Pass the file to PrintWriter
-        pw.println("Hello, World!"); // Write a line to the file
-        pw.println("This is a test file."); // Write another line
-        pw.print(100); // Write a number
-        pw.close(); // Close the PrintWriter
+        pw.println("Hello, World!"); 
+        pw.println("This is a test file."); 
+        pw.print(100); 
+        pw.close(); 
 
         // Read from the file using BufferedReader
         FileReader fr = new FileReader(file);
@@ -24,6 +24,6 @@ class test2 {
         while ((line = br.readLine()) != null) {
             System.out.println(line); // Print each line to the console
         }
-        br.close(); // Close the BufferedReader
+        br.close(); 
     }
 }
