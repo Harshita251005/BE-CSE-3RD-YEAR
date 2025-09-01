4fabe2e4ec5c4e5c4c38715ae53f9eee86ad5ef8
Harshita <parasgoyal299@gmail.com>
2025-09-01 11:08:19 +0530

    done

--- BEE/class28/routes/auth.route.js
+++ BEE/class28/routes/auth.route.js
@@ -6,98 +6,104 @@ const jwt = require("jsonwebtoken");
 const verifyAuth = require("../middleware/auth.middleware");
 const verifyAdmin = require("../middleware/admin.middleware");
 
-router.get("/signup",(req,res)=>{
+router.get("/signup", (req, res) => {
   res.render("signup");
-})
+});
 
-router.get("/login",(req,res)=>{
+router.get("/login", (req, res) => {
   res.render("login");
-})
+});
 
-router.post("/signup",async(req,res)=>{
+router.post("/signup", async (req, res) => {
   try {
-    const {name,email,password} = req.body;
-    if(!name || !email || !password){
-      throw new Error("All fields are required")
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      throw new Error("All fields are required");
     }
-    const hashPass = await bcrypt.hash(password,10)
+    const hashPass = await bcrypt.hash(password, 10);
 
     const user = await User.create({
-      name:name,
-      email:email,
-      password:hashPass
-    })
+      name: name,
+      email: email,
+      password: hashPass,
+    });
     res.redirect("/auth/login");
     // res.status(201).json({user,message:"user created successfully"})
   } catch (error) {
-    res.status(500).json({message:error.message})
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
-router.post("/admin/signup",async(req,res)=>{
+router.post("/admin/signup", async (req, res) => {
   try {
-    const {name,email,password} = req.body;
-    if(!name || !email || !password){
-      throw new Error("All fields are required")
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      throw new Error("All fields are required");
     }
-    const hashPass = await bcrypt.hash(password,10)
+    const hashPass = await bcrypt.hash(password, 10);
 
     const user = await User.create({
-      name:name,
-      email:email,
-      password:hashPass,
-      role:"admin"
-    })
-    res.status(201).json({user,message:"user created successfully"})
+      name: name,
+      email: email,
+      password: hashPass,
+      role: "admin",
+    });
+    res.status(201).json({ user, message: "user created successfully" });
   } catch (error) {
-    res.status(500).json({message:error.message})
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
-router.get("/info",verifyAuth,verifyAdmin,async (req,res)=>{
+router.get("/info", verifyAuth, verifyAdmin, async (req, res) => {
   try {
     // get all the users whose role is not "admin"
-    const users = await User.find({role:{$ne:"admin"}});
-    res.status(200).json({users})
+    const users = await User.find({ role: { $ne: "admin" } });
+    res.status(200).json({ users });
   } catch (error) {
-    res.status(500).json({message:error.message})
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
-router.post("/login",async(req,res)=>{
+router.post("/login", async (req, res) => {
   try {
-    const {email,password} = req.body;
-    if(!email || !password){
+    const { email, password } = req.body;
+    if (!email || !password) {
       throw new Error("all fields are required");
     }
-    const user = await User.findOne({email:email}).select("+password");
-    if(!user){
+    const user = await User.findOne({ email: email }).select("+password");
+    if (!user) {
       throw new Error("Invalid email or password");
     }
-    const isMatched = await bcrypt.compare(password,user.password);
-    if(!isMatched){
+    const isMatched = await bcrypt.compare(password, user.password);
+    if (!isMatched) {
       throw new Error("Invalid email or password");
     }
-    const token = jwt.sign({id:user._id,name:user.name},process.env.JWT_SECRET,
-      {expiresIn:'1h',algorithm:'HS256'}
+    const token = jwt.sign(
+      { id: user._id, name: user.name },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h", algorithm: "HS256" }
     );
-    res.cookie("token",token,{httpOnly:true,secure:true,maxAge:24*60*60*1000});
-    res.redirect("/");  // redirecting to home page
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.redirect("/"); // redirecting to home page
     // res.status(200).json({message:"You are loggedin"})
     // res.status(200).json({message:"You are loggedin",token:token})
   } catch (error) {
-    res.status(400).json({message:error.message});
+    res.status(400).json({ message: error.message });
   }
-})
+});
 
-router.get("/check",verifyAuth,async (req,res)=>{
+router.get("/check", verifyAuth, async (req, res) => {
   try {
     const userData = req.user;
     // res.send("hello");
-    res.status(200).json({user:userData});
+    res.status(200).json({ user: userData });
   } catch (error) {
-    res.status(400).json({message:error.message})
+    res.status(400).json({ message: error.message });
   }
-})
+});
 
 module.exports = router;
